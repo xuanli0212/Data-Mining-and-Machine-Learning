2566b3dca325c60e0aa3ed2703826bc0d8e2d012
xxxxxl <lix0212@hotmail.com>
2016-03-27 16:06:09 -0400

    Update DataProcessing.R

--- Project2/Rcodes/DataProcessing.R
+++ Project2/Rcodes/DataProcessing.R
@@ -1,117 +1,220 @@
+#Generate overall
+data$overalltime<-0.15*as.numeric(rownames(data))
+debora<-data[which(data$user=='debora'),]
+jose<-data[which(data$user=='jose_carlos'),]
+katia<-data[which(data$user=='katia'),]
+wallace<-data[which(data$user=='wallace'),]
 
-sensor1<-data[,c(1,2,3,13,14,15,25,26,27,37,38,39,49,53,54,55,65)]
-sensor2<-data[,c(4,5,6,16,17,18,28,29,30,40,41,42,50,56,57,58,65)]
-sensor3<-data[,c(7,8,9,19,20,21,31,32,33,43,44,45,51,59,60,61,65)]
-sensor4<-data[,c(10,11,12,22,23,24,34,35,36,46,47,48,52,62,63,64,65)]
 
-do.classification <- function(train.set, test.set,cl.name, verbose=F) {
-  ## note: to plot ROC later, we want the raw probabilities,
-  ## not binary decisions
-  switch(cl.name,
-         knn1= { # here we test k=1; you should evaluate different k's
-           prob = knn(train.set[,-65], test.set[,-65], cl=train.set[,65], k = 1, prob=F)
-           #attr(prob,"prob")[prob==0] = 1-attr(prob,"prob")[prob==0] #modified
-           #prob = attr(prob,"prob")
-           #print(cbind(prob,as.character(test.set$y)))
-           prob
-         },
-         
-         randomForest = {
-           model <- randomForest(class ~.,data = train.set)
-           if (verbose) {
-             print(summary(model)) # detailed summary of splits
-             ## plot the tree
-             plot(model, main=" Random Forest")
-           }           
-           
-           prob = predict(model, newdata=test.set,probability=F)
-           prob
-         },
-         randomForest1 = {
-           model <- randomForest(class ~aad.x4+avg.z1+avg.z2+aad.y4+range.z1+avg.y2+dist1+aad.y1+aad.x1+aad.x3,data = train.set)
-           if (verbose) {
-             print(summary(model)) # detailed summary of splits
-             ## plot the tree
-             plot(model, main=" Random Forest")
-           }           
-           
-           prob = predict(model, newdata=test.set,probability=F)
-           prob
-         },
-         
-         randomForest2 = {
-           model <- randomForest(class ~aad.x4+avg.z1+avg.z2+aad.y4+range.z1+avg.y2+dist1+aad.y1+aad.x1+aad.x3+aad.z1+ std.x3 +std.y1+aad.y3 +aad.z3+std.x4+range.z3+aad.z2 +dist3 +std.z1,data = train.set)
-           if (verbose) {
-             print(summary(model)) # detailed summary of splits
-             ## plot the tree
-             plot(model, main=" Random Forest")
-           }           
-           
-           prob = predict(model, newdata=test.set,probability=F)
-           prob
-         },
-         
-         
-         svm = {
-           model = svm(class~., data=train.set, probability=F)
-           prob = predict(model, newdata=test.set, probability=F)
-           #prob = attr(prob,"probabilities")
-           #print(cbind(prob,as.character(test.set$y)))
-           #print(dim(prob))
-           #prob = prob[,which(colnames(prob)==1)]/rowSums(prob)
-           prob
-         },
-         
-         svm.1 = {
-           model = svm(class~aad.x4+avg.z1+avg.z2+aad.y4+range.z1+avg.y2+dist1+aad.y1+aad.x1+aad.x3, data=train.set, probability=F)
-           prob = predict(model, newdata=test.set, probability=F)
-           #prob = attr(prob,"probabilities")
-           #print(cbind(prob,as.character(test.set$y)))
-           #print(dim(prob))
-           #prob = prob[,which(colnames(prob)==1)]/rowSums(prob)
-           prob
-         },
-        
-         svm.2 = {
-           model = svm(class~aad.x4+avg.z1+avg.z2+aad.y4+range.z1+avg.y2+dist1+aad.y1+aad.x1+aad.x3+aad.z1+ std.x3 +std.y1+aad.y3 +aad.z3+std.x4+range.z3+aad.z2 +dist3 +std.z1, data=train.set, probability=F)
-           prob = predict(model, newdata=test.set, probability=F)
-           #prob = attr(prob,"probabilities")
-           #print(cbind(prob,as.character(test.set$y)))
-           #print(dim(prob))
-           #prob = prob[,which(colnames(prob)==1)]/rowSums(prob)
-           prob
-         }, 
-         
-  ) 
+#time by person
+debora$rownumber <- 1:nrow(debora)
+jose$rownumber <- 1:nrow(jose)
+katia$rownumber <- 1:nrow(katia)
+wallace$rownumber <- 1:nrow(wallace)
+
+
+debora$timebyPerson<-0.15*debora$rownumber
+jose$timebyPerson<-0.15*jose$rownumber
+katia$timebyPerson<-0.15*katia$rownumber
+wallace$timebyPerson<-0.15*wallace$rownumber
+
+#time by each classification
+debora$timebyClass<-0.15
+jose$timebyClass<-0.15
+katia$timebyClass<-0.15
+wallace$timebyClass<-0.15
+
+  ###turn categorical to factor 
+library(plyr)
+debora$y <- mapvalues(debora$class, from = c("sitting", "sittingdown","standing","standingup","walking"), to = c("1", "2","3","4","5"))
+wallace$y <- mapvalues(wallace$class, from = c("sitting", "sittingdown","standing","standingup","walking"), to = c("1", "2","3","4","5"))
+jose$y <- mapvalues(jose$class, from = c("sitting", "sittingdown","standing","standingup","walking"), to = c("1", "2","3","4","5"))
+katia$y <- mapvalues(katia$class, from = c("sitting", "sittingdown","standing","standingup","walking"), to = c("1", "2","3","4","5"))
+
+#generate timebyClass
+time1<-debora$timebyClass
+time2<-jose$timebyClass
+time3<-katia$timebyClass
+time4<-wallace$timebyClass
+for(i in 1:(length(time1)-1)){
+  y<-debora$y
+  if(y[i]==y[i+1]){
+    time1[i+1]=time1[i]+0.15}
+    else{
+      time1[i+1]=0.15}
 }
+debora$timebyClass<-time1
 
-k.fold.cv <- function(dataset, cl.name, k.fold=10, prob.cutoff=0.5) {
-  ## default: 10-fold CV, cut-off 0.5 
-  n.obs <- nrow(dataset) # no. of observations 
-  s = sample(n.obs)
-  accuracy<-dim(10)
-  kappa<-dim(10)
-  actuals = NULL
-  for (k in 1:k.fold) {
-    test.idx = which(s %% k.fold == (k-1) ) # use modular operator
-    train.set = dataset[-test.idx,]
-    test.set = dataset[test.idx,]
-    cat(k.fold,'-fold CV run',k,cl.name,':',
-        '#training:',nrow(train.set),
-        '#testing',nrow(test.set),'\n')
-    prob = do.classification(train.set, test.set, cl.name)
-    actual = test.set$class
-    matrix<-confusionMatrix(prob,actual,dnn = c("Prediction", "Reference"))
-    accuracy[k]<-as.numeric(matrix$overall[1])
-    kappa[k]<-as.numeric(matrix$overall[2])
-  }
-  acc<-mean(accuracy)
-  kap<-mean(kappa)
-  print(acc)
-  print(kap)
+for(i in 1:(nrow(jose)-1)){
+  y<-jose$y
+  if(y[i]==y[i+1]){
+    time2[i+1]=time2[i]+0.15}
+  else{
+    time2[i+1]=0.15}
 }
-sensor1.random<-k.fold.cv(sensor1,'randomForest')
-sensor2.random<-k.fold.cv(sensor2,'randomForest')
-sensor3.random<-k.fold.cv(sensor3,'randomForest')
-sensor4.random<-k.fold.cv(sensor4,'randomForest')
+jose$timebyClass<-time2
+
+for(i in 1:(length(time3)-1)){
+  y<-katia$y
+  if(y[i]==y[i+1]){
+    time3[i+1]=time3[i]+0.15}
+  else{
+    time3[i+1]=0.15}
+}
+katia$timebyClass<-time3
+
+for(i in 1:(length(time4)-1)){
+  y<-wallace$y
+  if(y[i]==y[i+1]){
+    time4[i+1]=time4[i]+0.15}
+  else{
+    time4[i+1]=0.15}
+}
+wallace$timebyClass<-time4
+
+#Different Moving Pattern with x/y/z research
+
+
+sit<-debora[which(debora$y==1),]
+sitdata<-sit[100:150,]
+sitdata.matrix = sitdata[,8:19]
+sitdata.matrix=as.matrix(sitdata.matrix)
+class(sitdata.matrix)<-"numeric"
+sitdata.scaled = apply(sitdata.matrix,2,function(x)(x-mean(x)))
+
+sitdown<-debora[which(debora$y==2),]
+sddata<-sitdown[100:200,]
+sddata.matrix = as.matrix(sddata[,7:18])
+class(sddata.matrix)<-"numeric"
+sddata.scaled = apply(sddata.matrix,2,function(x)(x-mean(x)))
+
+stand<-debora[which(debora$y==3),]
+stdata<-stand[100:200,]
+stdata.matrix = as.matrix(stdata[,7:18])
+class(stdata.matrix)<-"numeric"
+stdata.scaled = apply(stdata.matrix,2,function(x)(x-mean(x)))
+
+standup<-debora[which(debora$y==4),]
+standupdata<-standup[100:200,]
+standupdata.matrix = as.matrix(standupdata[,7:18])
+class(standupdata.matrix)<-"numeric"
+standupdata.scaled = apply(standupdata.matrix,2,function(x)(x-mean(x)))
+
+walking<-debora[which(debora$y==5),]
+walkdata<-walking[100:150,]
+walkdata.matrix = as.matrix(walkdata[8:19])
+class(walkdata.matrix)<-"numeric"
+walkdata.scaled = apply(walkdata.matrix,2,function(x)(x-mean(x)))
+
+par(mfrow=c(1,2))
+plot(0,0,main="Sitting Pattern",xlab="time",ylab="Acceleration",xlim=c(0,15),ylim=c(-10,10))
+lines(sitdata$timebyClass,sitdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(sitdata$timebyClass,sitdata.scaled[,2],col="blue",lty=5,lwd=2)
+lines(sitdata$timebyClass,sitdata.scaled[,3],col="green",lty=3,lwd=2)
+legend(6,9,c("X","Y","Z"),lty=c(1,5,3),col=c("red","blue","green"),cex=0.5,horiz = TRUE)
+
+plot(0,0,main="Sitting-Down Pattern",xlab="time",ylab="Acceleration",xlim=c(0,15),ylim=c(-45,70))
+lines(sddata$timebyClass,sddata.scaled[,1],col="red",lty=1,lwd=2)
+lines(sddata$timebyClass,sddata.scaled[,2],col="blue",lty=5,lwd=2)
+lines(sddata$timebyClass,sddata.scaled[,3],col="green",lty=3,lwd=2)
+legend(6,70,c("X","Y","Z"),lty=c(1,5,3),col=c("red","blue","green"),cex=0.5,horiz = TRUE)
+
+par(mfrow=c(1,2))
+
+plot(0,0,main="Standing Pattern",xlab="time",ylab="Acceleration",xlim=c(0,15),ylim=c(-10,15))
+lines(sddata$timebyClass,stdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(sddata$timebyClass,stdata.scaled[,2],col="blue",lty=5,lwd=2)
+lines(sddata$timebyClass,stdata.scaled[,3],col="green",lty=3,lwd=2)
+legend(6,15,c("X","Y","Z"),lty=c(1,5,3),col=c("red","blue","green"),cex=0.5,horiz = TRUE)
+
+plot(0,0,main="Standing-Up Pattern",xlab="time",ylab="Acceleration",xlim=c(0,15),ylim=c(-50,60))
+lines(standupdata$timebyClass,standupdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(standupdata$timebyClass,standupdata.scaled[,2],col="blue",lty=5,lwd=2)
+lines(standupdata$timebyClass,standupdata.scaled[,3],col="green",lty=3,lwd=2)
+legend(6,60,c("X","Y","Z"),lty=c(1,5,3),col=c("red","blue","green"),cex=0.5,horiz = TRUE)
+
+par(mfrow=c(1,2))
+
+plot(0,0,main="Walking Pattern",xlab="time",ylab="Acceleration",xlim=c(0,15),ylim=c(-50,100))
+lines(walkdata$timebyClass,walkdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(walkdata$timebyClass,walkdata.scaled[,2],col="blue",lty=5,lwd=2)
+lines(walkdata$timebyClass,walkdata.scaled[,3],col="green",lty=3,lwd=2)
+legend(6,100,c("X","Y","Z"),lty=c(1,5,3),col=c("red","blue","green"),cex=0.5,horiz = TRUE)
+
+
+
+
+##Different sensor
+sitdata<-sit[100:150,]
+par(mfrow=c(1,3))
+plot(sitdata$timebyclass,sitdata.scaled[,10],pch=20,cex=0.8,col="orange",main="Sitting X ",xlab="time",ylab="X Acceleration",xlim=c(15,23),ylim=c(-7,13))
+lines(sitdata$timebyclass,sitdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,4],col="blue",lty=5,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,7],col="green",lty=3,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,10],col="orange",lty=1,lwd=1.5)
+legend(19,13,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=1)
+
+plot(sitdata$timebyclass,sitdata.scaled[,11],pch=20,cex=0.8,col="orange",main="Sitting Y",xlab="time",ylab="Y Acceleration",xlim=c(15,23),ylim=c(-7,13))
+lines(sitdata$timebyclass,sitdata.scaled[,2],col="red",lty=1,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,5],col="blue",lty=5,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,8],col="green",lty=3,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,11],col="orange",lty=1,lwd=1.5)
+#legend(19,13,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=0.5)
+
+plot(sitdata$timebyclass,sitdata.scaled[,12],pch=20,cex=0.8,col="orange",main="Sitting Z ",xlab="time",ylab="Z Acceleration",xlim=c(15,23),ylim=c(-7,13))
+lines(sitdata$timebyclass,sitdata.scaled[,3],col="red",lty=1,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,6],col="blue",lty=5,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,9],col="green",lty=3,lwd=2)
+lines(sitdata$timebyclass,sitdata.scaled[,12],col="orange",lty=1,lwd=1.5)
+#legend(19,13,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=0.5)
+
+
+
+
+plot(stdata$timebyClass,stdata.scaled[,10],pch=20,cex=0.8,col="orange",main="Sit-down with Different Sensors",xlab="time",ylab="Acceleration",xlim=c(15,30),ylim=c(-7,13))
+lines(stdata$timebyClass,stdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(stdata$timebyClass,stdata.scaled[,4],col="blue",lty=5,lwd=2)
+lines(stdata$timebyClass,stdata.scaled[,7],col="green",lty=3,lwd=2)
+lines(stdata$timebyClass,stdata.scaled[,10],col="orange",lty=1,lwd=1.5)
+legend(23,13,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=0.5)
+
+par(mfrow=c(1,2))
+plot(sddata$timebyClass,sddata.scaled[,10],pch=20,cex=0.8,col="orange",main="Standing with Different Sensors",xlab="time",ylab="Acceleration",xlim=c(15,30),ylim=c(-50,70))
+lines(sddata$timebyClass,sddata.scaled[,1],col="red",lty=1,lwd=2)
+lines(sddata$timebyClass,sddata.scaled[,4],col="blue",lty=5,lwd=2)
+lines(sddata$timebyClass,sddata.scaled[,7],col="green",lty=3,lwd=2)
+lines(sddata$timebyClass,sddata.scaled[,10],col="orange",lty=1,lwd=1.5)
+legend(23,70,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=0.5)
+
+plot(standupdata$timebyClass,standupdata.scaled[,10],pch=20,cex=0.8,col="orange",main="Stand-up with Different Sensors",xlab="time",ylab="Acceleration",xlim=c(15,30),ylim=c(-50,70))
+lines(standupdata$timebyClass,standupdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(standupdata$timebyClass,standupdata.scaled[,4],col="blue",lty=5,lwd=2)
+lines(standupdata$timebyClass,standupdata.scaled[,7],col="green",lty=3,lwd=2)
+lines(standupdata$timebyClass,standupdata.scaled[,10],col="orange",lty=1,lwd=1.5)
+legend(23,70,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=0.5)
+
+
+###
+par(mfrow=c(1,3))
+plot(walkdata$timebyclass,walkdata.scaled[,10],pch=20,cex=0.8,col="orange",main="Walking X",xlab="time",ylab="Acceleration",xlim=c(15,23),ylim=c(-100,150))
+lines(walkdata$timebyclass,walkdata.scaled[,1],col="red",lty=1,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,4],col="blue",lty=5,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,7],col="green",lty=3,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,10],col="orange",lty=1,lwd=1.5)
+legend(19,150,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=1)
+
+plot(walkdata$timebyclass,walkdata.scaled[,10],pch=20,cex=0.8,col="orange",main="Walking Y",xlab="time",ylab="Acceleration",xlim=c(15,23),ylim=c(-100,150))
+lines(walkdata$timebyclass,walkdata.scaled[,2],col="red",lty=1,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,5],col="blue",lty=5,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,8],col="green",lty=3,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,11],col="orange",lty=1,lwd=1.5)
+#legend(19,150,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=1)
+
+plot(walkdata$timebyclass,walkdata.scaled[,10],pch=20,cex=0.8,col="orange",main="Walking Z",xlab="time",ylab="Acceleration",xlim=c(15,23),ylim=c(-100,150))
+lines(walkdata$timebyclass,walkdata.scaled[,3],col="red",lty=1,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,6],col="blue",lty=5,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,9],col="green",lty=3,lwd=2)
+lines(walkdata$timebyclass,walkdata.scaled[,12],col="orange",lty=1,lwd=1.5)
+#legend(19,150,c("waist","left thigh","right arm","right ankle"),lty=c(1,5,3,1),col=c("red","blue","green","orange"),cex=1)
+
 
